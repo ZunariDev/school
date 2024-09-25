1f21ca6f43519cd0155d0264660985463f557a4a
Zunari <120125635+ZunariDev@users.noreply.github.com>
2024-09-25 15:52:28 +0200

    Update and rename bubblesort.py to bubbleSort.py

---
+++ bubbleSort.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+list = [i+1 for i in range(int(input("List length [-> int] > ")))]
+random.shuffle(list)
+
+def sort(item):
+    for i in range(1, len(item)):
+        for i in range(len(item) - 1):
+            if item[i] > item[i+1]:
+                item[i], item[i+1] = item[i+1], item[i]
+
+print(list)
+timeStart = time.time()
+sort(list)
+timeEnd = time.time()
+print(list)
+print(f"Sorting took {round(timeEnd - timeStart, 4)} seconds.")
